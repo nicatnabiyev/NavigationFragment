c8cd4d95551e9445bbb041908f8bfba7750be34c
nicatnabiyev <56915973+nicatnabiyev@users.noreply.github.com>
2021-07-13 12:43:07 +0400

    first version

---
+++ app/src/main/java/com/nicatnabiyev88/navigationfragments/SecondFragment.kt
@@ -0,0 +1,23 @@
+package com.nicatnabiyev88.navigationfragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.view.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        view.txt_second.setOnClickListener { Navigation.findNavController(view).navigate(R.id.navigateToFirstFragment) }
+        return view
+    }
+
+}
